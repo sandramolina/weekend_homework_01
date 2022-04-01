21763f37fb851a84e4822a4a7818d741f09b6d08
Sandra Molina Palencia <smolinapalencia@gmail.com>
2022-04-01 16:41:27 +0100

    completes add pet to customer fun

--- weekend_homework/start_point/src/pet_shop.py
+++ weekend_homework/start_point/src/pet_shop.py
@@ -46,3 +46,6 @@ def remove_customer_cash(customer, cash_to_rm):
 
 def get_customer_pet_count(customer):
     return len(customer["pets"])
+
+def add_pet_to_customer(customer, new_pet):
+    customer["pets"].append(new_pet)

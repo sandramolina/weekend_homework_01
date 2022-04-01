3523e1f1e336cb77e075aec333ed74208654d27f
Sandra Molina Palencia <smolinapalencia@gmail.com>
2022-04-01 14:52:42 +0100

    changes list parameter to pet shop list

--- weekend_homework/start_point/src/pet_shop.py
+++ weekend_homework/start_point/src/pet_shop.py
@@ -1,16 +1,18 @@
 # WRITE YOUR FUNCTIONS HERE 23 total
 
-def get_pet_shop_name(list):
-    return list["name"]
+def get_pet_shop_name(pet_shop_list):
+    return pet_shop_list["name"]
 
-def get_total_cash(list):
-    return list["admin"]["total_cash"]
+def get_total_cash(pet_shop_list):
+    return pet_shop_list["admin"]["total_cash"]
 
-def add_or_remove_cash(list, cash_in_out):
-    list["admin"]["total_cash"] += cash_in_out
+def add_or_remove_cash(pet_shop_list, cash_in_out):
+    pet_shop_list["admin"]["total_cash"] += cash_in_out
 
-def get_pets_sold(list):
-    return list["admin"]["pets_sold"]
+def get_pets_sold(pet_shop_list):
+    return pet_shop_list["admin"]["pets_sold"]
 
-def increase_pets_sold(list, new_pet_sold):
-    list["admin"]["pets_sold"] += new_pet_sold
+def increase_pets_sold(pet_shop_list, new_pet_sold):
+    pet_shop_list["admin"]["pets_sold"] += new_pet_sold
+
+# def get_stock_count():
